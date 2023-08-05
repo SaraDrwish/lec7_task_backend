8891b2d80f0263c8bee24f263c4fbf17d78edda1
Sara <38770404+SaraDrwish@users.noreply.github.com>
2023-08-06 00:15:22 +0300

    cmd 4

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,141 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    let form = document.getElementById("form1");
-    let addressF = document.getElementById("address");
+// const async = require("hbs/lib/async");  //returns promis
+  
+// -------------------------------------------------------------------------------------
+    // let form = document.getElementById("form1");
+    // let addressF = document.getElementById("address");
 
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        console.log(addressF.value)
-    })
+    // form.addEventListener("submit", (e) => {
+    //     e.preventDefault();
+    //     console.log(addressF.value)
+    //     weatherF();
+    //     form.reset();
+    // })
 
-    let errorF = document.getElementById("error");
-    let locationF = document.getElementById("location");
-    let forecastF = document.getElementById("forecast");
+    // let errorF = document.getElementById("error");
+    // let locationF = document.getElementById("location");
+    // let forecastF = document.getElementById("forecast");
 
     // let weatherF = async () => {
+        
+    //     try {
+    //         // const addresasncF = document.getElementById("address").value;
+    //         const addresasncF = addressF.value;
+    //         const res = await fetch("http://localhost:3000/weather?address="+addresasncF);
+    //         const data = await res.json()
+    //         console.log(data)
+    //         if (data.error) {
+    //             errorF.innerText = "there is an error here : " + data.error;
+    //             forecastF.innerText = "";
+    //             locationF.innerText = ""
+    //         } else {
+    //             locationF.innerText ="country name is :  " + data.location;
+    //             forecastF.innerText = "the forecast is : " + data.forecast;
+    //             errorF.innerText = ""
+    //         }
+    //     } catch(e){
+    //         console.log(e);
+    //     }
     
-
     // }
+// -------------------------------------------------------------------------------------
+
+
+// start task 7
+
+//    let form = document.getElementById("form1");
+//     let addressF = document.getElementById("address");
+    
+//     form.addEventListener("submit", (e) => {
+//         e.preventDefault();
+//         console.log(addressF.value)
+//         weatherF();
+//         form.reset();
+//     })
+
+//     let errorF = document.getElementById("error");
+//     let locationF = document.getElementById("location");
+//     let latitudeF = document.getElementById("latitude");
+//     let longitudeF = document.getElementById("longitude");
+//     let weatherStatusF = document.getElementById("weatherStatus");
+
+//     let weatherF = async () => {
+//         try {
+//             const addresasncF = addressF.value;
+//             const res = await fetch("http://localhost:3000/weather?address="+addresasncF);
+//             const data = await res.json()
+//             console.log(data)
+//             if (data.error) {
+//                 // await delay(500);
+//                 errorF.innerText =  "there is an error here : " + data.error;
+//                 locationF.innerText = "";
+//                 longitudeF.innerText = "";
+//                 latitudeF.innerText = "";
+//                 weatherStatusF.innerText = "";
+//             } else {
+//                 // await delay(500);
+//                 locationF.innerText ="country name is :  " + data.location;
+//                 latitudeF.innerText = "the forecast is : " + data.latitude;
+//                 longitudeF.innerText = "the forecast is : " + data.longitude;
+//                 weatherStatusF.innerText = "the forecast is : " + data.conditionText;
+//                 errorF.innerText = ""
+//             }
+//         } catch(e){
+//             console.log(e);
+//         }
+    
+//     }
+
+    
+    
+    // ---------------------------------------
+
+
+const form = document.getElementById("form1");
+const addressInput = document.getElementById("address");
+const locationParagraph = document.getElementById("location");
+const latitudeParagraph = document.getElementById("latitude");
+const longitudeParagraph = document.getElementById("longitude");
+const weatherStatusParagraph = document.getElementById("weatherStatus");
+const errorParagraph = document.getElementById("error");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const address = addressInput.value;
+
+    fetch(`/getWeather?address=${encodeURIComponent(address)}`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.json();
+        })
+        .then((data) => {
+            locationParagraph.textContent = data.location;
+            latitudeParagraph.textContent = `Latitude: ${data.latitude}`;
+            longitudeParagraph.textContent = `Longitude: ${data.longitude}`;
+            weatherStatusParagraph.textContent = data.forecastData;
+            errorParagraph.textContent = "";
+        })
+        .catch((error) => {
+            errorParagraph.textContent = "Error: " + error.message;
+            locationParagraph.textContent = "";
+            latitudeParagraph.textContent = "";
+            longitudeParagraph.textContent = "";
+            weatherStatusParagraph.textContent = "";
+        });
+});
+
+// -----------------end task 7 
+
+
+ 
+
+
+
+
 
 }
 )
--- src/app.js
+++ src/app.js
@@ -29,41 +29,51 @@ hbs.registerPartials(partialPath)
 
 app.get("/", (req, res) => {
     res.render("index", {
-        title: "home page",
-        desc: "lalalalalalalalalal home"
+        title: "welcome home , ",
+        desc: "write down the country that you want to know its weather now : "
     });
 })
 
-app.get("/contact", (req, res) => {
-    res.render("contact", {
-        title: "contact page",
-        desc: "contact contact contact "
-    })
-})
 
 // app.get("/weather", (req, res) => {
 //     res.render("weather", {
 //         title: "weather page"
-//         // data: data1,
- 
+//         // locationF,
+//         // latitudeF,
+//         // longitudeF,
+//         // weatherStatusF
 //     })
 
-//     //   res.render("weather", 
-//     //      <h3> la renderr rr  </h3> 
-//     // )
+    //   res.render("weather", 
+    //      <h3> la renderr rr  </h3> 
+    // )
     
-//     // res.send( <h3> send func </h3>)
+    // res.send( <h3> send func </h3>)
 
 // })
+
+// app.get("/weather", (req, res) => {
+//     res.json({
+//         title: "weather page"
+//     });
+// })
+
+
 
 app.get("/products", (req, res) => {
     // record the data of all   keys and values 
     console.log(req.query)
-    res.send(
-        {
-            product: "bmw", 
-            date: "2023"
-            }
+    // res.send(
+    //     {
+    //         product: "bmw", 
+    //         date: "2023"
+    //         }
+    // )
+    res.render(
+        "products" , {
+        product: "bmw",
+        date: "2023"
+         }
     )
 });
 
@@ -85,42 +95,93 @@ app.get("/products", (req, res) => {
 
 // --------------------------
 
-//task 7 --------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------
 
 const geocode = require("./Tools/geocode")
 const forecast = require("./Tools/forecast")
 
-app.get("/weather", (req, res) => {
-    console.log("Received request for /weather:", req.query);
-    if (!req.query.address) {
-        return res.send( {
-            error: "enter adress"
-        })
-    }
+// app.get("/weather", (req, res) => {
+//     console.log("Received request for /weather:", req.query);
+//     if (!req.query.address) {
+//         return res.send( {
+//             error: "enter adress"
+//         })
+//     }
 
-        geocode(req.query.address, (error, data) => {
-            if (error) { // geocode reorr 
-                return res.send({error})
-            }
-            forecast(data.latitude, data.longitude, (error, dataForecadt) => {
-                if (error) { //forecast erorr -- error
-                    return res.send({error})
-                }
-                res.send(
-                    {
-                        forecast:dataForecadt,
-                        location: req.query.address
-                    }
-                )
-            })
+//         geocode(req.query.address, (error, data) => {
+//             if (error) { // geocode reorr
+//                 return res.send({error})
+//             }
+//             forecast(data.latitude, data.longitude, (error, dataForecast) => {
+//                 if (error) { //forecast erorr -- error
+//                     return res.send({error})
+//                 }
+//                 res.send(
+//                     {
+//                         forecast:dataForecadt,
+//                         location: req.query.address
+//                     }
+//                 )
+//             })
 
-      })
+//       })
      
+// });
+
+// ------------------------------------task 7-----------------------------------------------------------------------------------
+
+  
+    app.get("/weather", (req, res) => {
+    res.render("weather", {
+        title: "Weather Page",
+        location: "",
+        latitude: "",
+        longitude: "",
+        forecastData: "",
+    });
+ 
+
 });
 
 
+app.get("/getWeather", (req, res) => {
+    const address = req.query.address; // Get the address from the query parameter
 
-//task 7 end --------------------------------------------------------------------------------------------------------------
+    if (!address) {
+        return res.status(400).json({ error: "Please provide an address" });
+    }
+
+    // Use the geocode function to fetch the latitude, longitude, and location based on the address.
+    geocode(address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.status(500).json({ error });
+        }
+
+        // Use the forecast function to fetch the weather data based on the latitude and longitude.
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.status(500).json({ error });
+            }
+
+            // Return the weather data as JSON response.
+            res.json({
+                location,
+                latitude,
+                longitude,
+                forecastData,
+            });
+        });
+    });
+});
+
+// ---------------------------------------------
+
+
+ 
+
+// ---------------------------------------------
+
+
 
 
 
